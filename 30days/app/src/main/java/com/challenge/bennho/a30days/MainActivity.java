9f8788c610e677a2ebf3f3fb9ec7d5909f43c4f3
benj0505 <ben0505@hotmail.my>
2016-12-20 15:03:30 +0800

    detail

--- 30days/app/src/main/java/com/challenge/bennho/a30days/MainActivity.java
+++ 30days/app/src/main/java/com/challenge/bennho/a30days/MainActivity.java
@@ -1,13 +1,31 @@
 package com.challenge.bennho.a30days;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnBegin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnBegin = (Button) findViewById(R.id.btnBegin);
+
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, PersonalDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ 30days/app/src/main/java/com/challenge/bennho/a30days/PersonalDetailActivity.java
@@ -0,0 +1,15 @@
+package com.challenge.bennho.a30days;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_detail);
+
+        this.setTitle("dsa");
+    }
+}
