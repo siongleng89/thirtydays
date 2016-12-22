5b126f91262f94825969303cf7187bee6ea9e49d
benj0505 <ben0505@hotmail.my>
2016-12-22 15:12:35 +0800

    i learn so muchhhhhhhhhhhhhhh

---
+++ 30days/app/src/main/java/com/challenge/bennho/a30days/activity_end.java
@@ -0,0 +1,13 @@
+package com.challenge.bennho.a30days;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_end extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+    }
+}
--- 30days/app/src/main/java/com/challenge/bennho/a30days/controls/BottomBar.java
+++ 30days/app/src/main/java/com/challenge/bennho/a30days/controls/BottomBar.java
@@ -1,8 +1,42 @@
 package com.challenge.bennho.a30days.controls;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.challenge.bennho.a30days.R;
+
 /**
  * Created by Dell-PC on 22/12/2016.
  */
 
-public class BottomBar {
+public class BottomBar extends RelativeLayout{
+
+    private Context context;
+
+
+    public BottomBar(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BottomBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public BottomBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init (Context context){
+        this.context = context;
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.layout_bottom_button, this, true);
+    }
 }
